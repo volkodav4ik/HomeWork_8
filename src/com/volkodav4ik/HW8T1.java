2ac088d640ea374933a2daba93e609fda0ae27dd
Artem Vovk <volkodav4ik1990@gmail.com>
2019-12-06 17:36:48 +0200

    Add task 1

---
+++ src/com/volkodav4ik/HW8T1.java
@@ -0,0 +1,22 @@
+package com.volkodav4ik;
+
+public class HW8T1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 99; i++) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                System.out.print("HelloWorld ");
+            } else {
+                if ((i % 3) == 0) {
+                    System.out.print("Hello ");
+                } else {
+                    if ((i % 5) == 0) {
+                        System.out.print("World ");
+                    } else {
+                        System.out.print(i + " ");
+                    }
+                }
+            }
+        }
+    }
+}
